f72007bfb404377449081507396552aba152ffb7
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-15 03:05:46 +1100

    Add header.css and header.jsx files

---
+++ frontend/src/header.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './header.css'
+
+function Header() {
+    return (
+        <header>
+            <h1></h1>
+        </header>
+    )
+}
+
+export default Header;
