bd5c7f6840a898bf255f3ce4edb268c996317095
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-15 03:41:24 +1100

    Add basic header hyperlinks and user input for account search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import Header from './Header'; // Importing the default export from Header.jsx
+import Header from './Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>
+        <Header />
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -9,6 +9,27 @@ function Header() {
             <a href="/leaderboard">Leaderboard</a>
             <a href="/statistics">Statistics</a>
             <a href="/queue_types">Queue Types</a>
+            <form>
+                <select>
+                    <option value="">North America (NA)</option>
+                    <option value="">Europe West (EUW)</option>
+                    <option value="">Europe Nordic & East (EUNE)</option>
+                    <option value="">Oceania (OCE)</option>
+                    <option value="">Russia (RU)</option>
+                    <option value="">Turkey (TR)</option>
+                    <option value="">Brazil (BR)</option>
+                    <option value="">Latin America North (LAN)</option>
+                    <option value="">Latin America South (LAS)</option>
+                    <option value="">Japan (JP)</option>
+                    <option value="">Taiwan (TW)</option>
+                    <option value="">Singapore (SG)</option>
+                    <option value="">Thailand (TH)</option>
+                    <option value="">Philippines (PH)</option>
+                    <option value="">Middle East (ME)</option>
+                </select>
+                <input type="text" name="username" placeholder="Enter username"></input>
+            </form>
+            <a href = "/user">Log In</a>
         </nav>
     )
 }
