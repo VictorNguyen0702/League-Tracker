e513808433e83f77414691bd36fb7fa35ed82d75
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-17 03:40:26 +1100

    Add helper functions to get puu id from riot id and encrypted summoner id from puu id

--- backend/api_calls.py
+++ backend/api_calls.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 API_key = os.getenv("RIOT_API_KEY")
 
-regions_dict = {
+region_dict = {
     "NA": "na1",
     "EUW": "euw1",
     "EUNE": "eun1",
@@ -24,35 +24,57 @@ regions_dict = {
     "ME": "mea1"
 }
 
-def get_league_by_queue(tier: str, queue: str, region: str, division: str = "I"):
+queue_dict = {
+    "solo": "RANKED_SOLO_5x5",
+    "flex": "RANKED_FLEX_SR"
+}
+
+
+def get_puuid(riot_id:str):
+    """
+    Returns the puuid of a riot account depending on riot id
+    """
+    gameName, tagLine = riot_id.split("#")
+    url = f"https://americas.api.riotgames.com//riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}"
+
+    response = requests.get(url, headers = {"X-Riot-Token": API_key})
+    if response.status_code == 200:
+        return response.json()['puuid']
+    else:
+        print(f"Error getting PUU ID: {response.status_code}")
+        return None
+    
+
+def get_encrypted_summoner_id(riot_id: str, region: str) -> str:
+    """
+    Returns the summoner id of a summoner 
+    """
+
+    region_param = region_dict[region]
+    puu_id = get_puuid(riot_id)
+
+    url = f"https://{region_param}.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puu_id}"
+
+    response = requests.get(url, headers = {"X-Riot-Token": API_key})
+    if response.status_code == 200:
+        return response.json()["id"]
+    else:
+        print(f"Error getting summoner ID: {response.status_code}")
+        return None
+
+
+def get_league_by_queue(tier: str, queue: str, region: str, division: str = "I") -> dict | None:
     """
     Returns a list of players in a certain rank of a queue type for one region
-
-    Parameters:
-    league (str): The rank to be filtered by (master, grandmaster or challenger)
-    queue (str): The type of queue (ranked solo or flex)
-    region (str): The region in which the search is done
-    division (str): The division of the rank (only applies for lower than master)
-
-    Returns:
-    Dictionary: The data of summoners in the league and queue type
     """
 
-    queue_param = ""
-    if queue == "solo":
-        queue_param = "RANKED_SOLO_5x5"
-    elif queue == "flex":
-        queue_param = "RANKED_FLEX_SR"
-
-    region_param = regions_dict[region]
-    
+    queue_param, region_param = queue_dict[queue], region_dict[region]
     if tier in ["master", "grandmaster", "challenger"]:
         url = f"https://{region_param}.api.riotgames.com/lol/league/v4/{tier}leagues/by-queue/{queue_param}"
     else:
         url = f"https://{region_param}.api.riotgames.com/lol/league/v4/entries/{queue_param}/{tier.upper()}/{division}"
-    
-    response = requests.get(url, headers = {"X-Riot-Token": API_key})
 
+    response = requests.get(url, headers = {"X-Riot-Token": API_key})
     if response.status_code == 200:
         return response.json()
     else:
