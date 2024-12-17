7de01f6cd00432da46bf247705c392e7e5a125de
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-17 18:53:47 +1100

    Add profile photo and login/profile link

--- frontend/src/header.jsx
+++ frontend/src/header.jsx
@@ -2,16 +2,29 @@ import { useState } from 'react'
 import './header.css'
 
 function Header() {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [username, setUsername] = useState('john_doe'); 
+    let userLink;
+    if (isLoggedIn) {
+        userLink = { href: `/${username}`, text: "Profile Page"};
+    } else {
+        userLink = { href: "/login", text: "Log In"};
+    }
+
     return (
         <div id="header">
             <nav>
-                <a href="/" className="Header Link">Home</a>
-                <a href="/champions" className="Header Link">Champions</a>
-                <a href="/leaderboard" className="Header Link">Leaderboard</a>
-                <a href="/statistics" className="Header Link">Statistics</a>
-                <a href="/queue_types" className="Header Link">Queue Types</a>
+                <a href="/" className="header-link">Home</a>
+                <a href="/champions" className="header-link">Champions</a>
+                <a href="/leaderboard" className="header-link">Leaderboard</a>
+                <a href="/statistics" className="header-link">Statistics</a>
+                <a href="/queue_types" className="header-link">Queue Types</a>
             </nav>
-            <a href="/user" className="Header Link">User Page Placeholder</a>
+            <div id="user-page">
+                <img src="/imgs/profile-photo.png" alt="Logo" className="profile-image" />
+                <a href={userLink.href} className="user-link">{userLink.text}</a>
+            </div>
         </div>
     )
 }
