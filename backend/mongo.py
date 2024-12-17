1bc923b5cb3688e0596c90fef086e9d0934b8ea8
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-17 17:06:45 +1100

    Add function to retrieve leaderboard from mongoDB Atlas

--- backend/mongo.py
+++ backend/mongo.py
@@ -10,8 +10,21 @@ mongoURL = os.getenv("MONGO_URL")
     
 client = MongoClient(mongoURL)
 
+def find_leaderboard(region: str, queue: str, tier: str, division: str) -> dict:
+    leaderboard = client["ranks"]["leaderboard"]
 
-def save_leaderboard(region: str, queue: str, tier: str, division: str = "I") -> None:
+    result = leaderboard.find_one(
+        {"region": region,
+         f"queues.{queue}.tiers.{tier}.divisions.{division}": {"$exists": True}})
+    
+    if result:
+        users_dict = result.get("queues").get(queue).get("tiers").get(tier).get("divisions").get(division).get("users", {})
+        return users_dict
+    else:
+        return {}
+
+
+def download_leaderboard(region: str, queue: str, tier: str, division: str = "I") -> None:
     leaderboard = client["ranks"]["leaderboard"]
 
     new_users = api_calls.get_league_by_queue(region, queue, tier, division)
@@ -24,3 +37,4 @@ def save_leaderboard(region: str, queue: str, tier: str, division: str = "I") ->
         },
         upsert = True
     )
+
