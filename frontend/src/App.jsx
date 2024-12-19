b648364f455d3cc5a83c60bd040cb062cc064663
Victor Nguyen <victornguyen0702@gmail.com>
2024-12-20 03:20:01 +1100

    Changed dropdown menus to MUI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,11 @@ import Header from "./components/Header/Header.jsx";
 import Leaderboard from "./pages/Leaderboard/Leaderboard.jsx";
 import Home from "./pages/Home/Home.jsx"
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 function App() {
     const [count, setCount] = useState(0)
 
