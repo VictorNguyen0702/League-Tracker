82b32cdb4a6a1ce2e0bbe49e776e4bd089d9c0cd
Victor Nguyen <victornguyen0702@gmail.com>
2025-03-16 17:41:00 +1100

    fix: fixed backend

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # Replace "*" with your React app's domain in production
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
